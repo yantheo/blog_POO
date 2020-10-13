c4e504f81315f00db24fdc02f25646af173000ab
Yanick Theodose <yanick.theodose@outlook.com>
2020-10-13 18:41:44 +0200

    Test Demo Design Pattern Façade

--- Blog/app/Controller/DemoController.php
+++ Blog/app/Controller/DemoController.php
@@ -2,14 +2,14 @@
 
 namespace App\Controller;
 
-use Core\Database\QueryBuilder;
+//use Core\Database\QueryBuilder;
 
 class DemoController extends AppController{
 
     public function index(){
-        $query = new QueryBuilder();
-        echo $query
-            ->select('id', 'titre', 'contenu')
+        require ROOT . '/Query.php';
+        //$query = new QueryBuilder();
+        echo \Query::select('id', 'titre', 'contenu')
             ->from('articles', 'Post')
             ->where('Post.category_id = 1')
             ->where('Post.date > NOW()');
